2e99d54be5b639d05a7db9b59224f75fe6864a35
Flynn Fehre <flyfeh@outlook.de>
2024-06-24 19:49:53 +0200

    First draft of DrawCyclicDoubleCoverToTikz finished

--- gap/tikz/drawing.gd
+++ gap/tikz/drawing.gd
@@ -1,2 +1,3 @@
 DeclareOperation( "DrawDigraphToTikz", [IsDigraph, IsString, IsRecord]);
 DeclareOperation( "DrawStraightPlanarDigraphToTikz", [IsDigraph ,IsString,IsRecord]);
+DeclareOperation( "DrawCycleDoubleCoverToTikz", [IsDigraph ,IsString,IsRecord]);
